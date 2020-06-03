f1cc8b28b8d28e007a67a400994eede01fe6e884
Ramakrishnan Kandasamy <rmkanda@thoughtworks.com>
2020-06-03 11:06:07 +0530

    [Ramki] Add html response for / endpoint

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -2,9 +2,17 @@ package com.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+	@RequestMapping(value = "/")
+	public String available() {
+	return "<html><body><h1>Sample Spring Application</body></h1></html>";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
